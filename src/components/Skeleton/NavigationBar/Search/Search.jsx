bcef5abbfa87a59255f48e522eac877808902876
Klementeva Katya <klementevak2000@inbox.ru>
2021-11-15 00:36:46 +0300

    Небольшие поправки в стилях

--- src/components/Skeleton/NavigationBar/Search/Search.jsx
+++ src/components/Skeleton/NavigationBar/Search/Search.jsx
@@ -6,7 +6,7 @@ function Search(props) {
     return (
         <div>
             <div className={styles.search}>
-                <div className={styles.text}>Поиск</div>
+                <div className={styles.text}><h3>Поиск</h3></div>
                 <SearchInput toggleFilterMode={props.toggleFilterMode} setSearchText={props.setSearchText}/>
             </div>
         </div>
