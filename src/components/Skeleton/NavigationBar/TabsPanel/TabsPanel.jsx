2563864822c2b7dd631803097a91301d1a621310
Klementeva Katya <klementevak2000@inbox.ru>
2021-11-14 22:20:59 +0300

    Доделана адаптивность и отзывчивость

--- src/components/Skeleton/NavigationBar/TabsPanel/TabsPanel.jsx
+++ src/components/Skeleton/NavigationBar/TabsPanel/TabsPanel.jsx
@@ -4,21 +4,24 @@ import styles from "./TabsPanel.module.css";
 import {NavLink} from 'react-router-dom';
 function TabsPanel() {
     return (
-        <div className={styles.panel}>
-            <NavLink to="/"><span>Все</span></NavLink>
-            <NavLink to="/design"><span>Designers</span></NavLink>
-            <NavLink to="/analytics"><span>Analysts</span></NavLink>
-            <NavLink to="/frontend"><span>Frontend</span></NavLink>
-            <NavLink to="/backend"><span>Backend</span></NavLink>
-            <NavLink to="/ios"><span>IOS</span></NavLink>
-            <NavLink to="/android"><span>Android</span></NavLink>
-            <NavLink to="/qa"><span>QA</span></NavLink>
-            <NavLink to="/hr"><span>HR</span></NavLink>
-            <NavLink to="/management"><span>Managers</span></NavLink>
-            <NavLink to="/back_office"><span>Back Office</span></NavLink>
-            <NavLink to="/support"><span>Support</span></NavLink>
-            <NavLink to="/pr"><span>PR</span></NavLink>
+        <div className={styles.panelWrapper}>
+                <div className={styles.panel}>
+                        <NavLink to="/"><span>Все</span></NavLink>
+                        <NavLink to="/design"><span>Designers</span></NavLink>
+                        <NavLink to="/analytics"><span>Analysts</span></NavLink>
+                        <NavLink to="/frontend"><span>Frontend</span></NavLink>
+                        <NavLink to="/backend"><span>Backend</span></NavLink>
+                        <NavLink to="/ios"><span>IOS</span></NavLink>
+                        <NavLink to="/android"><span>Android</span></NavLink>
+                        <NavLink to="/qa"><span>QA</span></NavLink>
+                        <NavLink to="/hr"><span>HR</span></NavLink>
+                        <NavLink to="/management"><span>Managers</span></NavLink>
+                        <NavLink to="/back_office"><span>Back Office</span></NavLink>
+                        <NavLink to="/support"><span>Support</span></NavLink>
+                        <NavLink to="/pr"><span>PR</span></NavLink>
+                </div>
         </div>
+
     );
 }
 export default TabsPanel;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import store from "./redux/redux-store";
+import MetaTags from 'react-meta-tags';
 
 ReactDOM.render(
-
     <Provider store={store}>
+        <MetaTags>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        </MetaTags>
         <App />
     </Provider>,
     document.getElementById('root')
