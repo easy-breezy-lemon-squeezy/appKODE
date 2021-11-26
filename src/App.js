723f6c32b5e1c71c20799f0304cabd9aa129af2e
Klementeva Katya <klementevak2000@inbox.ru>
2021-11-26 23:58:25 +0300

    hash router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import SkeletonContainer from "./components/Skeleton/SkeletonContainer";
 import React from "react";
 
 function App() {
 
   return (
-      <BrowserRouter>
+      <HashRouter>
           <div className="App">
               <SkeletonContainer/>
           </div>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
